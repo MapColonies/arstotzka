18772c201799cce07eb96d568e6f2d35a3fea0b8
melancholiai <samit2310@gmail.com>
2023-01-29 16:21:20 +0200

    fix(configurations): fixed commitlint extends

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,7 @@
 const { utils: { getPackages } } = require('@commitlint/config-lerna-scopes');
 
 module.exports = {
+   extends: ['@commitlint/config-conventional'],
    rules: {
       'scope-enum': async ctx =>
           [2, 'always', [...(await getPackages(ctx)), "deps", "configurations"]]
