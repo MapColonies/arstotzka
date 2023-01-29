34f38a9ad3fc28aba630ca96157f8a126ebff54b
melancholiai <samit2310@gmail.com>
2023-01-29 16:06:39 +0200

    configurations: as lerna monorepo

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,8 @@
+const { utils: { getPackages } } = require('@commitlint/config-lerna-scopes');
+
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        "scope-enum": [2, "always", ["deps", "configurations"]],
-    }
+   rules: {
+      'scope-enum': async ctx =>
+          [2, 'always', [...(await getPackages(ctx)), "deps", "configurations"]]
+   }
 };
